cdab2bea4b58a30766c963a0651bcc519559d11c
Dyyynn <muhsyaifudin51@gmail.com>
2023-03-18 11:56:13 +0700

    Main Commit

---
+++ resources/js/Pages/Home.vue
@@ -0,0 +1,15 @@
+<script setup>
+    import MainLayout from '@/Layouts/MainLayout.vue';
+</script>
+
+<template>
+    <Head title="Welcome" />
+
+    <MainLayout>
+        
+    </MainLayout>
+
+</template>
+
+
+
